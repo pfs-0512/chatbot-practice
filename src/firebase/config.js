1ed43b85ed64a8587f547965f1b310534c1d7144
katakawaryouhei <kyatakawa1230@gmail.com>
2020-06-01 23:24:40 +0900

    set-github

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB49CxSAkrOicBLRxV-wPU9oScK4VWVATQ",
+  authDomain: "chatbot-app-de428.firebaseapp.com",
+  databaseURL: "https://chatbot-app-de428.firebaseio.com",
+  projectId: "chatbot-app-de428",
+  storageBucket: "chatbot-app-de428.appspot.com",
+  messagingSenderId: "250870721391",
+  appId: "1:250870721391:web:f8001f5507d962483d169a",
+  measurementId: "G-0TQZWDHNWG"
+};
+
+export default firebaseConfig;
